b1f01200934b9c62315414fef96a913d915144d6
David Cortarello <david.cortarello@accenture.com>
2019-06-30 18:42:09 -0300

    ping code borrowed from https://github.com/corentone/python3-ping

--- bin/tint2_stats.py
+++ bin/tint2_stats.py
@@ -5,7 +5,133 @@ import math
 import multiprocessing
 
 import socket, struct
-from ping3 import ping
+
+import select, time
+
+class ping:
+    ICMP_ECHO_REQUEST = 8
+
+    def checksum(self, source_string):
+        """
+        I'm not too confident that this is right but testing seems
+        to suggest that it gives the same answers as in_cksum in ping.c
+        """
+        sum = 0
+        countTo = (len(source_string)/2)*2
+        count = 0
+        while count<countTo:
+            thisVal = source_string[count + 1]*256 + source_string[count]
+            sum = sum + thisVal
+            sum = sum & 0xffffffff # Necessary?
+            count = count + 2
+
+        if countTo<len(source_string):
+            sum = sum + source_string[len(source_string) - 1]
+            sum = sum & 0xffffffff # Necessary?
+
+        sum = (sum >> 16)  +  (sum & 0xffff)
+        sum = sum + (sum >> 16)
+        answer = ~sum
+        answer = answer & 0xffff
+
+        # Swap bytes. Bugger me if I know why.
+        answer = answer >> 8 | (answer << 8 & 0xff00)
+
+        return answer
+
+
+    def receive_one_ping(self, my_socket, ID, timeout):
+        """
+        receive the ping from the socket.
+        """
+        timeLeft = timeout
+        while True:
+            startedSelect = time.time()
+            whatReady = select.select([my_socket], [], [], timeLeft)
+            howLongInSelect = (time.time() - startedSelect)
+            if whatReady[0] == []: # Timeout
+                return
+
+            timeReceived = time.time()
+            recPacket, addr = my_socket.recvfrom(1024)
+            icmpHeader = recPacket[20:28]
+            type, code, checksum, packetID, sequence = struct.unpack(
+                "bbHHh", icmpHeader
+            )
+            # Filters out the echo request itself.
+            # This can be tested by pinging 127.0.0.1
+            # You'll see your own request
+            if type != 8 and packetID == ID:
+                bytesInDouble = struct.calcsize("d")
+                timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+                return timeReceived - timeSent
+
+            timeLeft = timeLeft - howLongInSelect
+            if timeLeft <= 0:
+                return
+
+
+    def send_one_ping(self, my_socket, dest_addr, ID):
+        """
+        Send one ping to the given >dest_addr<.
+        """
+        dest_addr  =  socket.gethostbyname(dest_addr)
+
+        # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+        my_checksum = 0
+
+        # Make a dummy heder with a 0 checksum.
+        header = struct.pack("bbHHh", self.ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1)
+        bytesInDouble = struct.calcsize("d")
+        data = bytes((192 - bytesInDouble) * "Q", 'utf-8')
+        data = struct.pack("d", time.time()) + data
+
+        # Calculate the checksum on the data and the dummy header.
+        my_checksum = self.checksum(header + data)
+
+        # Now that we have the right checksum, we put that in. It's just easier
+        # to make up a new header than to stuff it into the dummy.
+        header = struct.pack(
+            "bbHHh", self.ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), ID, 1
+        )
+        packet = header + data
+        my_socket.sendto(packet, (dest_addr, 1)) # Don't know about the 1
+
+
+    def do_one(self, dest_addr, timeout):
+        """
+        Returns either the delay (in seconds) or none on timeout.
+        """
+        icmp = socket.getprotobyname("icmp")
+        try:
+            my_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
+        except PermissionError as e:
+            e.args = (e.args if e.args else tuple()) + ((
+                " - Note that ICMP messages can only be sent from processes"
+                " running as root."
+            ),)
+            raise
+
+        my_ID = os.getpid() & 0xFFFF
+
+        self.send_one_ping(my_socket, dest_addr, my_ID)
+        delay = self.receive_one_ping(my_socket, my_ID, timeout)
+
+        my_socket.close()
+        return delay
+
+    def get_ping_time(self, myaddr):
+        try:
+            delay = self.do_one(myaddr, 2)
+        except socket.gaierror as e:
+            print("failed. (socket error: '%s')" % e)
+            return None
+
+        if delay is None:
+            print("failed. (timeout within %ssec.)" % timeout)
+        else:
+            delay = delay
+        return delay
 
 def root_usage():
     disk = os.statvfs("/")
@@ -42,7 +168,8 @@ def latency_to_gateway():
                 if fields[1] != '00000000' or not int(fields[3], 16) & 2:
                     continue
                 gateway = socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
-                return str(math.ceil(ping(gateway).real * 1000))
+                myping = ping()
+                return str(math.ceil(myping.get_ping_time(gateway).real * 1000))
     except:
         pass
     return None
